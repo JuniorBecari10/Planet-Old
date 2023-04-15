d643dc9967b7e37946163c13f4b95c04d9eef077
Antônio Carlos <juniorbecari@gmail.com>
2023-04-15 20:52:46 -0300

    feat: added Open Base Folder button

--- frontend/src/components/WelcomeScreen.vue
+++ frontend/src/components/WelcomeScreen.vue
@@ -16,6 +16,15 @@
 	<div class="container">
 		<img src="../assets/images/png/planet.png">
 		<SearchBar placeholder="Search and Create" />
+
+		<div class="button">
+			<a href="">
+				<img src="../assets/images/svg/new-folder.svg">
+				<span>Open Base Folder</span>
+			</a>
+		</div>
+
+		<h3>Recent Folders</h3>
 	</div>
 </template>
 
@@ -28,13 +37,35 @@
 
 		display: flex;
 
-		align-items: center;
+		align-items: left;
 		justify-content: center;
 		flex-direction: column;
 	}
 
-	img {
+	div.container > *:not(img) {
+		margin-top: 20px;
+	}
+
+	.container > img {
 		width: 50%;
-		margin-bottom: 20px;
+
+		margin-left: 50%;
+		transform: translateX(-50%);
+	}
+
+	.button {
+		width: 100%;
+	}
+
+	.button a {
+		display: flex;
+		width: 100%;
+
+		align-items: center;
+	}
+
+	.button img {
+		width: 32px;
+		margin-right: 20px;
 	}
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,3 +2,11 @@ import {createApp} from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+// custom code
+
+var anchors = document.querySelectorAll('a');
+
+for (let i = 0; i < anchors.length; i++) {
+  anchors[i].addEventListener('click', e => e.preventDefault(), false);
+}
