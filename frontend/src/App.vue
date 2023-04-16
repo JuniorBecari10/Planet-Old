53dd1ee52f82068b853fb1f448f76d12897478e1
Antônio Carlos <juniorbecari@gmail.com>
2023-04-16 10:52:29 -0300

    feat: adding functions

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,18 @@
-<script setup>
+<script setup lang="ts">
+	import { ref, Ref } from "vue";
+
+	var baseFolder: Ref<string> = ref("");
 	
+	function setBaseFolder(path: string): void {
+
+	}
+
+	var functions: Ref<{}> = ref({
+		setBaseFolder: setBaseFolder,
+	});
 </script>
 
-<script>
+<script lang="ts">
 	import WelcomeScreen from "./components/WelcomeScreen.vue";
 
 	export default {
